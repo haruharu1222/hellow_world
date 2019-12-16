43b8a747be68e327e4677d65febc44e6248c2367
haru <haruhi.12.yamaguchi.22@gmail.com>
2019-12-17 06:09:42 +0900

    画像のボタン化成功。クリックするとランダムに移動。

--- santa_tatch/java/com/example/santa_tatch/MainActivity.java
+++ santa_tatch/java/com/example/santa_tatch/MainActivity.java
@@ -1,4 +1,105 @@
 package com.example.santa_tatch;
+//AndroidX
+import androidx.appcompat.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private LinearLayout.LayoutParams buttonLayoutParams;
+    private  ImageButton imageButton;
+    private int scale;
+    private int marginR;
+    private int marginU;
+    private int marginL;
+    private int marginD;
+
+    Random r = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // リニアレイアウトの設定
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        int mParent = LinearLayout.LayoutParams.MATCH_PARENT;
+        int wContent = LinearLayout.LayoutParams.WRAP_CONTENT;
+
+        layout.setLayoutParams(new LinearLayout.LayoutParams(mParent, mParent));
+
+        // レイアウト中央寄せ
+        layout.setGravity(Gravity.CENTER);
+        // 背景色
+        layout.setBackgroundColor(Color.parseColor("#ffdfcf"));
+
+        setContentView(layout);
+
+        scale =  TypedValue.COMPLEX_UNIT_DIP;
+        // ボタンの設定
+        imageButton = new ImageButton(this);
+
+        //ボタンサイズ
+        int dpH = (int) TypedValue.applyDimension(scale, 200, getResources().getDisplayMetrics());
+        int dpD = (int) TypedValue.applyDimension(scale, 100, getResources().getDisplayMetrics());
+        buttonLayoutParams = new LinearLayout.LayoutParams(dpH, dpD);
+
+        // margin
+        int margin10 = 10 * scale;
+        buttonLayoutParams.setMargins(margin10, margin10, margin10, margin10);
+
+        // ボタンに画像を設定
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.santa);
+        Drawable drawable = new BitmapDrawable(getResources(), bitmap);
+
+        // Bitmapで画像を設定
+        imageButton.setImageBitmap(bitmap);
+
+        imageButton.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        imageButton.setBackgroundColor(Color.parseColor("#00000000"));
+
+        imageButton.setLayoutParams(buttonLayoutParams);
+        layout.addView(imageButton);
+
+        // リスナーをボタンに登録
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                marginL=(int) (r.nextInt(300))*scale;
+                marginU=(int) (r.nextInt(800))*scale;
+                marginR=(int) (r.nextInt(300))*scale;
+                marginD=(int) (r.nextInt(800))*scale;
+                buttonLayoutParams.setMargins(marginL, marginU, marginR, marginD);
+                imageButton.setLayoutParams(buttonLayoutParams);
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*
 // androidx
 import androidx.appcompat.app.AppCompatActivity;
 // no androidx
@@ -110,5 +211,6 @@ public class MainActivity extends AppCompatActivity {
         button.setLayoutParams(buttonLayoutParams);
         textView.setText(R.string.hello);
     }
- */
+
 }
+ */
